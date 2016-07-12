16e4d70dbbd446656752619e2a12ea498c9988a1
pamelaolney <pjolney12@gmail.com>
2016-07-12 13:41:08 -0400

    updated page, almost bronze level

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 $(document).ready(function() {
+//flip the flashcard
   $('.card').on('click', function() {
     $('.card').toggleClass('flipped');
   });
-  //variables
-  // var questionNum = 0;
-  // var answerNum = 0;
-  var questions = new Array('Q1', 'Q2', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 'Q15', 'Q16', 'Q17', 'Q18', 'Q19', 'Q20');
-  var answers = new Array('A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20');
-  var buttoninorder = $('#inorder');
-  var clickCounter = 0
-
-  function handleClickEvent() {
-    console.log('ive been clicked');
-  }
-    buttoninorder.on("click", handleClickEvent );
 
 
+//tabs for flashcard game
+    $('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = $(this).attr('href');
+
+        // Show/Hide Tabs
+        $('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
 
 
+//next & prev button
+
+
+
+
+
+
+
+
+
+
+
+//prompt user to imput content onto card
+
+
+// //hide the button after click
+//   $('#beginlearning').click(function () {
+//            $(this).hide();
+//   });
 
 });
