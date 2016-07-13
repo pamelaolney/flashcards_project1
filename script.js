3ebee734dac5e6ca8cb3b4b99f956bd8a06d9fa8
pamelaolney <pjolney12@gmail.com>
2016-07-13 01:06:48 -0400

    most up to date flashcards

--- script.js
+++ script.js
@@ -25,29 +25,103 @@ $(document).ready(function() {
 //prompt user to imput content onto card
 
 
-var question1 = {
-  question:'Whats my name?'
-},
+
+var flashcards =[
+  {question: "(Question)",
+   answer: "(Answer)"}
+]
+//--------tab 1 input ----------------|
+$("#tab1front").html(flashcards[0].question)
+
+$(".insertQuestion").on("click", function(){
+  var insertQuestionButton= prompt("please input your question or term here");
+  $("#tab1front").eq(0).html(insertQuestionButton).css("font-size", "40px");
+})
+
+$("#tab1back").html(flashcards[0].answer)
+
+$(".insertAnswer").on("click", function(){
+  var insertAnswerButton= prompt("please input your answer here");
+  $("#tab1back").eq(0).html(insertAnswerButton).css("font-size", "32px");
+})
+
+//--------tab 2 input -----------------|
+
+$("#tab2front").html(flashcards[0].question)
+
+$(".insertQuestion2").on("click", function(){
+  var insertQuestionButton= prompt("please input your question or term here");
+  $("#tab2front").eq(0).html(insertQuestionButton).css("font-size", "40px");
+})
+
+$("#tab2back").html(flashcards[0].answer)
+
+$(".insertAnswer2").on("click", function(){
+  var insertAnswerButton= prompt("please input your answer here");
+  $("#tab2back").eq(0).html(insertAnswerButton).css("font-size", "32px");
+})
 
 
+//--------tab 3 input -----------------|
 
+$("#tab3front").html(flashcards[0].question)
 
+$(".insertQuestion3").on("click", function(){
+  var insertQuestionButton= prompt("please input your question or term here");
+  $("#tab3front").eq(0).html(insertQuestionButton).css("font-size", "40px");
+})
 
+$("#tab3back").html(flashcards[0].answer)
 
+$(".insertAnswer3").on("click", function(){
+  var insertAnswerButton= prompt("please input your answer here");
+  $("#tab3back").eq(0).html(insertAnswerButton).css("font-size", "32px");
+})
 
+//--------tab 4 input -----------------|
 
-// //hide the button after click
-//   $('#beginlearning').click(function () {
-//            $(this).hide();
-//   });
+$("#tab4front").html(flashcards[0].question)
+
+$(".insertQuestion4").on("click", function(){
+  var insertQuestionButton= prompt("please input your question or term here");
+  $("#tab4front").eq(0).html(insertQuestionButton).css("font-size", "40px");
+})
+
+$("#tab4back").html(flashcards[0].answer)
+
+$(".insertAnswer4").on("click", function(){
+  var insertAnswerButton= prompt("please input your answer here");
+  $("#tab4back").eq(0).html(insertAnswerButton).css("font-size", "32px");
+})
+
+//--------tab 5 input -----------------|
+
+$("#tab5front").html(flashcards[0].question)
+
+$(".insertQuestion5").on("click", function(){
+  var insertQuestionButton= prompt("please input your question or term here");
+  $("#tab5front").eq(0).html(insertQuestionButton).css("font-size", "40px");
+})
+
+$("#tab5back").html(flashcards[0].answer)
+
+$(".insertAnswer5").on("click", function(){
+  var insertAnswerButton= prompt("please input your answer here");
+  $("#tab5back").eq(0).html(insertAnswerButton).css("font-size", "32px");
+})
+
+//hide the music after click
+  $('#hidemusic').click(function () {
+           $("iframe").hide();
+  });
+
+//show the music after click
+  $('#showmusic').click(function () {
+           $("iframe").show();
+  });
+
 
 });
 
-//
-//new way of flipping through flashcards
-//
-// var flashcards =[
-//   {question: {prompt("please input question here")} , answer: "yes"}
-// ]
-//
-// $("div.front").html(flashcard[0].question)
+
+// new way of flipping through flashcards
