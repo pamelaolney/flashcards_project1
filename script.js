d90c793e0a3846781189e6eeadbb26c0f2e28cdb
pamelaolney <pjolney12@gmail.com>
2016-09-04 05:04:42 -0400

    fixed typeo

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
 //next & prev button
 
 
+
+
+
 //prompt user to imput content onto card
 
 
@@ -30,83 +33,83 @@ var flashcards =[
   {question: "(Question)",
    answer: "(Answer)"}
 ]
-//--------tab 1 input ----------------|
+//--------tab 1 insert ----------------|
 $("#tab1front").html(flashcards[0].question)
 
 $(".insertQuestion").on("click", function(){
-  var insertQuestionButton= prompt("please input your question or term here");
+  var insertQuestionButton= prompt("Please insert your question or term here");
   $("#tab1front").eq(0).html(insertQuestionButton).css("font-size", "40px");
 })
 
 $("#tab1back").html(flashcards[0].answer)
 
 $(".insertAnswer").on("click", function(){
-  var insertAnswerButton= prompt("please input your answer here");
+  var insertAnswerButton= prompt("Please insert your answer here");
   $("#tab1back").eq(0).html(insertAnswerButton).css("font-size", "32px");
 })
 
-//--------tab 2 input -----------------|
+//--------tab 2 insert -----------------|
 
 $("#tab2front").html(flashcards[0].question)
 
 $(".insertQuestion2").on("click", function(){
-  var insertQuestionButton= prompt("please input your question or term here");
+  var insertQuestionButton= prompt("Please insert your question or term here");
   $("#tab2front").eq(0).html(insertQuestionButton).css("font-size", "40px");
 })
 
 $("#tab2back").html(flashcards[0].answer)
 
 $(".insertAnswer2").on("click", function(){
-  var insertAnswerButton= prompt("please input your answer here");
+  var insertAnswerButton= prompt("Please insert your answer here");
   $("#tab2back").eq(0).html(insertAnswerButton).css("font-size", "32px");
 })
 
 
-//--------tab 3 input -----------------|
+//--------tab 3 insert -----------------|
 
 $("#tab3front").html(flashcards[0].question)
 
 $(".insertQuestion3").on("click", function(){
-  var insertQuestionButton= prompt("please input your question or term here");
+  var insertQuestionButton= prompt("Please insert your question or term here");
   $("#tab3front").eq(0).html(insertQuestionButton).css("font-size", "40px");
 })
 
 $("#tab3back").html(flashcards[0].answer)
 
 $(".insertAnswer3").on("click", function(){
-  var insertAnswerButton= prompt("please input your answer here");
+  var insertAnswerButton= prompt("Please insert your answer here");
   $("#tab3back").eq(0).html(insertAnswerButton).css("font-size", "32px");
 })
 
-//--------tab 4 input -----------------|
+//--------tab 4 insert -----------------|
 
 $("#tab4front").html(flashcards[0].question)
 
 $(".insertQuestion4").on("click", function(){
-  var insertQuestionButton= prompt("please input your question or term here");
+  var insertQuestionButton= prompt("Please insert your question or term here");
   $("#tab4front").eq(0).html(insertQuestionButton).css("font-size", "40px");
 })
 
 $("#tab4back").html(flashcards[0].answer)
 
 $(".insertAnswer4").on("click", function(){
-  var insertAnswerButton= prompt("please input your answer here");
+  var insertAnswerButton= prompt("Please insert your answer here");
   $("#tab4back").eq(0).html(insertAnswerButton).css("font-size", "32px");
 })
 
-//--------tab 5 input -----------------|
+//--------tab 5 insert -----------------|
 
 $("#tab5front").html(flashcards[0].question)
 
 $(".insertQuestion5").on("click", function(){
-  var insertQuestionButton= prompt("please input your question or term here");
+  var insertQuestionButton= prompt("Please insert your question or term here");
   $("#tab5front").eq(0).html(insertQuestionButton).css("font-size", "40px");
 })
 
 $("#tab5back").html(flashcards[0].answer)
 
 $(".insertAnswer5").on("click", function(){
-  var insertAnswerButton= prompt("please input your answer here");
+  var insertAnswerButton= prompt("Please insert your answer here");
   $("#tab5back").eq(0).html(insertAnswerButton).css("font-size", "32px");
 })
 
@@ -122,6 +125,3 @@ $(".insertAnswer5").on("click", function(){
 
 
 });
-
-
-// new way of flipping through flashcards
